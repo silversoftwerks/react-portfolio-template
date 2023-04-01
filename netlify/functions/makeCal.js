1a66232f269e269446f4ca38b34a5ce2fef8545b
silversoftwerks <51791643+silversoftwerks@users.noreply.github.com>
2023-03-31 21:04:20 -0600

    Update and rename makeCal-background.js to makeCal.js

--- netlify/functions/makeCal.js
+++ netlify/functions/makeCal.js
@@ -6,7 +6,7 @@ import moment from "moment";
 // Here we use debug level of logging to improve the debugging experience.
 // This functionality is optional!
 log.setLevel(LogLevel.DEBUG);
-exports.handler = function (event, context) {
+exports.handler = async function (event, context) {
 
 // Create an instance of the CheerioCrawler class - a crawler
 // that automatically loads the URLs and parses their HTML using the cheerio library.
